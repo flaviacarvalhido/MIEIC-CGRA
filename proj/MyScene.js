6cc8ca582c29b3300ffd95c802c3a85ed3525796
flaviacarvalhido <flaviacarvalhido@gmail.com>
2020-04-13 11:27:34 +0100

    sphere done

--- proj/MyScene.js
+++ proj/MyScene.js
@@ -1,77 +1,90 @@
 /**
-* MyScene
-* @constructor
-*/
+ * MyScene
+ * @constructor
+ */
 class MyScene extends CGFscene {
-    constructor() {
-        super();
-    }
-    init(application) {
-        super.init(application);
-        this.initCameras();
-        this.initLights();
+  constructor() {
+    super();
+  }
+  init(application) {
+    super.init(application);
+    this.initCameras();
+    this.initLights();
 
-        //Background color
-        this.gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    //Background color
+    this.gl.clearColor(0.0, 0.0, 0.0, 1.0);
 
-        this.gl.clearDepth(100.0);
-        this.gl.enable(this.gl.DEPTH_TEST);
-        this.gl.enable(this.gl.CULL_FACE);
-        this.gl.depthFunc(this.gl.LEQUAL);
+    this.gl.clearDepth(100.0);
+    this.gl.enable(this.gl.DEPTH_TEST);
+    this.gl.enable(this.gl.CULL_FACE);
+    this.gl.depthFunc(this.gl.LEQUAL);
 
-        this.setUpdatePeriod(50);
-        
-        this.enableTextures(true);
+    this.setUpdatePeriod(50);
 
-        //Initialize scene objects
-        this.axis = new CGFaxis(this);
-        this.incompleteSphere = new MySphere(this, 16, 8);
+    this.enableTextures(true);
 
-        //Objects connected to MyInterface
-        this.displayAxis = true;
-    }
-    initLights() {
-        this.lights[0].setPosition(15, 2, 5, 1);
-        this.lights[0].setDiffuse(1.0, 1.0, 1.0, 1.0);
-        this.lights[0].enable();
-        this.lights[0].update();
-    }
-    initCameras() {
-        this.camera = new CGFcamera(0.4, 0.1, 500, vec3.fromValues(15, 15, 15), vec3.fromValues(0, 0, 0));
-    }
-    setDefaultAppearance() {
-        this.setAmbient(0.2, 0.4, 0.8, 1.0);
-        this.setDiffuse(0.2, 0.4, 0.8, 1.0);
-        this.setSpecular(0.2, 0.4, 0.8, 1.0);
-        this.setShininess(10.0);
-    }
-    // called periodically (as per setUpdatePeriod() in init())
-    update(t){
-        //To be done...
-    }
+    //Initialize scene objects
+    this.axis = new CGFaxis(this);
 
-    display() {
-        // ---- BEGIN Background, camera and axis setup
-        // Clear image and depth buffer everytime we update the scene
-        this.gl.viewport(0, 0, this.gl.canvas.width, this.gl.canvas.height);
-        this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
-        // Initialize Model-View matrix as identity (no transformation
-        this.updateProjectionMatrix();
-        this.loadIdentity();
-        // Apply transformations corresponding to the camera position relative to the origin
-        this.applyViewMatrix();
-        
-        // Draw axis
-        if (this.displayAxis)
-            this.axis.display();
+    this.sphere = new MySphere(this, 16, 8);
+    this.sphereMaterial = new CGFappearance(this);
+    this.sphereMaterial.setAmbient(0.1, 0.1, 0.1, 1);
+    this.sphereMaterial.setDiffuse(0.9, 0.9, 0.9, 1);
+    this.sphereMaterial.setSpecular(0.1, 0.1, 0.1, 1);
+    this.sphereMaterial.setShininess(10.0);
+    this.sphereMaterial.loadTexture("images/earth.jpg");
+    this.sphereMaterial.setTextureWrap("Repeat", "Clamp to edge");
 
-        this.setDefaultAppearance();
+    //Objects connected to MyInterface
+    this.displayAxis = true;
+  }
+  initLights() {
+    this.lights[0].setPosition(15, 2, 5, 1);
+    this.lights[0].setDiffuse(1.0, 1.0, 1.0, 1.0);
+    this.lights[0].enable();
+    this.lights[0].update();
+  }
+  initCameras() {
+    this.camera = new CGFcamera(
+      0.4,
+      0.1,
+      500,
+      vec3.fromValues(15, 15, 15),
+      vec3.fromValues(0, 0, 0)
+    );
+  }
+  setDefaultAppearance() {
+    this.setAmbient(0.2, 0.4, 0.8, 1.0);
+    this.setDiffuse(0.2, 0.4, 0.8, 1.0);
+    this.setSpecular(0.2, 0.4, 0.8, 1.0);
+    this.setShininess(10.0);
+  }
+  // called periodically (as per setUpdatePeriod() in init())
+  update(t) {
+    //To be done...
+  }
 
-        // ---- BEGIN Primitive drawing section
+  display() {
+    // ---- BEGIN Background, camera and axis setup
+    // Clear image and depth buffer everytime we update the scene
+    this.gl.viewport(0, 0, this.gl.canvas.width, this.gl.canvas.height);
+    this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
+    // Initialize Model-View matrix as identity (no transformation
+    this.updateProjectionMatrix();
+    this.loadIdentity();
+    // Apply transformations corresponding to the camera position relative to the origin
+    this.applyViewMatrix();
 
-        //This sphere does not have defined texture coordinates
-        this.incompleteSphere.display();
+    // Draw axis
+    if (this.displayAxis) this.axis.display();
 
-        // ---- END Primitive drawing section
-    }
+    this.setDefaultAppearance();
+
+    // ---- BEGIN Primitive drawing section
+
+    this.sphereMaterial.apply();
+    this.sphere.display();
+
+    // ---- END Primitive drawing section
+  }
 }
